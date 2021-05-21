bacc2ed5deceed36c040bba3fa7ad32b7a4bb2ad
Zepee <ramp8a799@gmail.com>
2021-05-22 01:59:40 +0300

    Function for search in tree addded

--- main.cpp
+++ main.cpp
@@ -75,11 +75,11 @@ vector<Spot> readSpotList(string path){
     return geoSpots;
 }
 
-bool PointIntersect(Vector2f point,Rectangle rec)
+bool PointIntersect(Spot *point, Rectangle rec)
 {
     double x1 = rec.LB.x,y1 = rec.LB.y;
     double x2 = rec.RT.x,y2 = rec.RT.y;
-    if(point.x < x1 || point.x > x2 || point.y < y1 || point.y > y2)
+    if(point->longitude < x1 || point->longitude > x2 || point->latitude < y1 || point->latitude > y2)
         return false;
     return true;
 }
@@ -89,4 +89,25 @@ bool RectIntersect(Rectangle rect1,Rectangle rect2)
     if ((rect1.LB.x <= rect2.RT.x || rect1.RT.x >= rect2.LB.x) && (rect1.LB.y <= rect2.RT.y || rect1.RT.y >= rect2.LB.y))
         return true;
     return false;
+}
+
+void findPointsInRect(Node* node, Rectangle rect, string type, string subtype, vector< Spot* >& result) {
+    if (node->childs.size() > 0 || node->isLeaf== false) {
+        if (RectIntersect(node->MBR, rect)) {
+            for (int i = 0; i < node->childs.size(); i++) {
+                findPointsInRect(node->childs[i], rect, type, subtype, result);
+            }
+        }
+    }
+    else
+        for(auto i = 0 ;i<node->data.size();i++)
+        {
+
+                if (PointIntersect(node->data[i], rect) && node->data[i]->type == type && node->data[i]->subtype == subtype)
+                {
+                    result.push_back(node->data[i]);
+                }
+
+        }
+
 }
